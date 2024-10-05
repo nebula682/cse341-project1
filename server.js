a1199227ad07ea91e1bff1cb14b4ca5bc15bef0d
nebula682 <malcolmnie@gmail.com>
2024-10-05 13:23:55 +0200

    PROJECT

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const mongodb = require("./data/database");
+const express = require('express');
+const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use("/", require("./routes"));
+app.use('/', require('./routes'));
 
 mongodb.initDb((err) =>{
                     if (err){
